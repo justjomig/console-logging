2ec2f9d9ef4b40b67186ae0e4c6a0c995caf7efe
Jose Miguel C Evangelista <jomigevangelista117@gmail.com>
2021-02-07 16:26:37 +0800

    Push to github -initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Log to console 
+
+console.log('HELLO WORLD');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+greeting = "How are you today";
+console.log(greeting);
+console.log([1,2,3,4,5]);
+
+console.log({a: 1, b: 2});
+console.table({a: 1, b: 2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('HELLO');
+console.log('Hello Earth');
+console.log('Hello Earth');
+console.log('Hello Earth');
+console.timeEnd('HELLO');
